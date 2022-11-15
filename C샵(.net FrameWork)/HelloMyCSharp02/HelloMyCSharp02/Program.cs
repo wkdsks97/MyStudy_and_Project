05e34d5d7796c9bc981b78459ac18997116db5ec
wkdsks97 <109117993+wkdsks97@users.noreply.github.com>
2022-11-15 16:40:39 +0900

    d
    d

---
+++ C샵(.net FrameWork)/HelloMyCSharp02/HelloMyCSharp02/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp02
+{
+    enum 가위바위보
+    {
+       // 가위,바위=500,보
+       가위,바위,보
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1번");
+            Console.WriteLine("안녕하세요");
+
+            Console.WriteLine("2번");
+            Console.WriteLine(10+20);
+            Console.WriteLine(7/3);//2
+            Console.WriteLine(7/3.0);//소수점 나옴 2.333
+            Console.WriteLine(7%3);
+            Console.WriteLine(7-3);
+            Console.WriteLine(10*20);
+
+            Console.WriteLine("3번");
+            Console.WriteLine("10"+10);//1010
+            Console.WriteLine('2'+100);//150 '2'=50
+            Console.WriteLine("10"+'2'); //102
+
+            Console.WriteLine("4번문제 입력받기");
+            int mybirth = int.Parse(Console.ReadLine());
+            Console.WriteLine("나는 " + mybirth + "생입니다.");
+
+            Console.WriteLine("5번문제 입력받기");
+            string myname = Console.ReadLine();
+            Console.WriteLine("내 이름은 " + myname+"입니다.");
+
+            Console.WriteLine("6번");
+            int age = 30;
+            string name = "이동준";
+            Console.WriteLine("나는 \""+name +"\"입니다. \""+age+"\"대입니다.");
+            Console.WriteLine($"나는 \"{name}\"입니다. \"{age}\"대입니다.");
+
+            Console.WriteLine("7번");
+            int myage = int.Parse(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.Year-myage+1);
+            
+            Console.WriteLine("8번");
+            Console.WriteLine("현재 몇년도인가?");
+            int nowYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("태어난 건 몇년도인가?");
+            int bornYear = int.Parse(Console.ReadLine());
+            Console.WriteLine(nowYear-bornYear+1);
+
+            Console.WriteLine("9번");
+            string input = Console.ReadLine();
+            Console.WriteLine(input[0]);
+
+            Console.WriteLine("10번");
+            int w = int.Parse(Console.ReadLine());
+            Console.WriteLine(w*w);
+
+            Console.WriteLine("11번");
+            int mynum = int.Parse(Console.ReadLine());
+            if(mynum<0)
+                Console.WriteLine("음수");
+            
+            Console.WriteLine("12번");
+            int myinput = int.Parse(Console.ReadLine());
+            if(myinput>0)
+                Console.WriteLine("자연수");
+            else
+                Console.WriteLine("자연수 아님");
+
+            Console.WriteLine("13번");
+            Random r = new Random();
+            int com = r.Next(3);//0이상 3미만의 숫자 아무거나
+            if(com==0)
+                Console.WriteLine("가위");
+            else if(com==1)
+                Console.WriteLine("바위");
+            else if(com==2)
+                Console.WriteLine("보"); //끝에 else를 안 적고 else if로 끝내도 됨
+
+            Console.WriteLine("14번");
+            int mychoice = int.Parse(Console.ReadLine());
+
+            //상수를 쓴다.
+            const int 가위 = 0;
+            Console.WriteLine(가위바위보.가위); //가위 그대로 출력됨
+            switch (mychoice)
+            {
+                case 가위: //가위
+                    switch (com)
+                    {
+                        case 가위:
+                            Console.WriteLine("비김");
+                            break;
+                        case 1:
+                            Console.WriteLine("패배");
+                            break;
+                        case 2:
+                            Console.WriteLine("승리");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case (int)가위바위보.바위: //바위
+                    switch (com)
+                    {
+                        case 가위:
+                            Console.WriteLine("승리");
+                            break;
+                        case 1:
+                            Console.WriteLine("비김");
+                            break;
+                        case 2:
+                            Console.WriteLine("패배");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case (int)가위바위보.보: //보
+                    switch (com)
+                    {
+                        case 가위:
+                            Console.WriteLine("패배");
+                            break;
+                        case 1:
+                            Console.WriteLine("승리");
+                            break;
+                        case 2:
+                            Console.WriteLine("비김");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("잘못된 값");
+                    break;
+            }
+
+            Console.WriteLine("15번");
+            if (com==mychoice)
+                Console.WriteLine("비김");
+            else if( (mychoice==(int)가위바위보.가위 && com== (int)가위바위보.보) 
+                || (mychoice==1 && com==가위) 
+                ||(mychoice == 2 && com == 1) ) //가위(0) -> 보(2), 바위(1) -> 가위(0), 보(2) -> 바위(1)
+                Console.WriteLine("이김");
+            else
+                Console.WriteLine("짐");
+
+            Console.WriteLine("16번");
+            for(int i = 1; i<=10; i++)
+                Console.WriteLine(i);
+
+            Console.WriteLine("17번");
+            int sum = 1;//sum=0;
+            for (int i = 1; i <= 10; i++)
+                sum *= i;
+
+            Console.WriteLine("18번");
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine($"안녕하세요 {i}번째 손님");
+
+            Console.WriteLine("19번");
+            while(true)
+            {
+                Console.WriteLine("숫자 입력");
+                int m = int.Parse(Console.ReadLine());
+                if (m == 0)
+                    break; //반복문 종료
+                Console.WriteLine(m*m);
+            }
+            Console.WriteLine("20-1번");
+
+            sum = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                if(i%2!=0)
+                    sum *= i;
+            }
+            Console.WriteLine("sum="+sum);
+            Console.WriteLine("20-2번");
+            sum = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                sum *= i;
+            }
+            Console.WriteLine("sum=" + sum);
+
+        }
+    }
+}
---
+++ C샵(.net FrameWork)/HelloMyCSharp02/HelloMyCSharp02_01/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp02_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string b = "안녕";
+            char c = 'A';
+            double d = 3.14;
+            bool e = false;
+
+            //1개의 변수인 데, 여러 개의 값을 갖게 하고 싶다면?
+            //ex) 학생
+            //이름값 = "오세룡"
+            //나이 = 20
+            //성별 = 남
+            //시력 = 0.01
+            //학점 = 'A'
+            //결혼여부 = false
+            //MBTI = "ISTJ"
+
+            int 나이_오세룡 = 20;
+            string 이름_오세룡 = "오세룡";
+            char 학점_오세룡 = 'A';
+            bool 결혼_오세룡 = false;
+            string MBTI_오세룡 = "ISTJ";
+            string 성별_오세룡 = "남";
+            double 시력_오세룡 = 0.01;
+
+            int 나이_조영탁 = 20;
+            string 이름_조영탁 = "오세룡";
+            char 학점_조영탁 = 'A';
+            bool 결혼_조영탁 = false;
+            string MBTI_조영탁 = "ISTJ";
+            string 성별_조영탁 = "남";
+            double 시력_조영탁 = 0.01;
+
+            //하나의 변수로 여러 개의 값을
+            //저장할 수 있는 자료형
+            //=> 사용자 정의 자료형
+            //int등과 같이 해당 언어에서 만든 자료형이
+            //아니고, 프로그래머가 직접 만들었기 때문.
+            //C -> 구조체(struct)
+            //Java -> class
+            //c++,C# -> struct, class
+
+            //c 언어에서는 사용자 정의 자료형이라고 함
+            //C++ C#, Java에서는 객체라는 말을 쓴다.
+            //사용자 정의 객체 혹은 그냥 객체라고 함
+
+            Student s1 = new Student();
+            s1.Name = "오세룡";
+            s1.Age = 20;
+            s1.eye = 0.01;
+            Student[] students = new Student[2];
+            students[0] = s1;
+            students[1] = new Student();
+            students[1].Name = "박지호";
+            students[1].Age = 25;
+            students[1].eye = 0.1;
+
+            Console.WriteLine(s1.Name);
+            Console.WriteLine(students[1].Name);
+            Console.WriteLine();
+
+            //1. Student 이용해서 학생 1명을 만든다.
+            //Name, Age, eye 값도 추가한다.
+            Student stu1 = new Student();
+            stu1.Name = "이동준";
+            stu1.Age = 34;
+            stu1.eye = 0.4;
+            Console.WriteLine($"{stu1.Name}학생은" +
+                $"{stu1.Age}살이고, 시력은 {stu1.eye}이다.");
+            // 2.Student 이용해서 학생 1명을 만들고
+            // 이 번엔 Console.ReadLine으로 값 받기
+            Student stu2 = new Student();
+            Console.WriteLine("이름은?");
+            stu2.Name = Console.ReadLine();
+            Console.WriteLine("나이는?");
+            stu2.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("시력은?");
+            stu2.eye = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{stu2.Name}학생은 " +
+                $"{stu2.Age}살이고, 시력은 {stu2.eye}입니다.");
+
+            Student[] stus = new Student[3];
+            stus[0] = new Student();
+            stus[0].Name = "조영탁";
+            stus[0].Age = 30;
+            stus[0].eye = 1.2;
+            stus[1] = new Student();
+            stus[1].Name = "이범식";
+            stus[1].Age = 35;
+            stus[1].eye = 1.6;
+            stus[2] = new Student();
+            stus[2].Name = "박재형";
+            stus[2].Age = 25;
+            stus[2].eye = 0.6;
+
+            for(int i = 0; i<stus.Length; i++)
+            {
+                Console.WriteLine($"{stus[i].Name}학생은" +
+                    $" {stus[i].Age}살이고, 시력은 {stus[i].eye}이다.");
+            }
+
+            Student[] stus2 = new Student[3];
+            for(int i = 0; i<stus2.Length; i++)
+            {
+                stus2[i] = new Student();
+                Console.WriteLine("이름?");
+                stus2[i].Name = Console.ReadLine();
+                Console.WriteLine("나이?");
+                stus2[i].Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("시력?");
+                stus2[i].eye = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("이름 :" + stus2[i].Name);
+                Console.WriteLine("나이 :" + stus2[i].Age);
+                Console.WriteLine("시력 :" + stus2[i].eye.ToString("0.0"));
+            }
+
+
+            Console.WriteLine("KB 학생 만들기!");
+            KBStudent kb1 = new KBStudent();
+            kb1.Name = "장혜정";
+            kb1.Age = 20;
+            kb1.Eye = 1.5;
+            kb1.MBTI = "ISFP";
+
+            Console.WriteLine("이름 : " + kb1.Name);
+            Console.WriteLine("나이 : " + kb1.Age);
+            Console.WriteLine("시력 : " + kb1.Eye);
+            Console.WriteLine("mbti : " + kb1.MBTI);
+
+            KBStudent kb2 = new KBStudent();
+            kb2.Name = "김성환";
+            kb2.Age = 20;
+            kb2.Eye = 1.7;
+            kb2.MBTI = "ENFP";
+            kb2.introduce(); //kb2의 모든 정보를 다 출력함
+
+        }
+    }
+}
