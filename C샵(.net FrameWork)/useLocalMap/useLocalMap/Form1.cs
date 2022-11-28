c2690c75e234b7ddf94813786a78812d62ae6edc
wkdsks97 <109117993+wkdsks97@users.noreply.github.com>
2022-11-28 14:08:40 +0900

    upload

---
+++ C샵(.net FrameWork)/useLocalMap/useLocalMap/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace useLocalMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //textBox1 위에 키보드를 누르고 그 키가 올라올 때
+            //이벤트 추가
+            //람다 사용함
+            //키가 엔터일 때, button1을 눌러준다.
+            textBox1.KeyUp += (sender, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                    button1.PerformClick();
+            }; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Locale> locales = KakaoAPI.Search(textBox1.Text);
+            listBox1.Items.Clear();
+            foreach(Locale item in locales)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            Locale ml = listBox1.SelectedItem as Locale; //선택한 것을 Locale로 변환
+            object[] pos = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser1.Document;
+            hdoc.InvokeScript("setCenter", pos);
+        }
+    }
+}
---
+++ C샵(.net FrameWork)/useLocalMap/useLocalMap/KakaoAPI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace useLocalMap
+{
+    public class KakaoAPI
+    {
+        //클래스 메소드 만듦
+        //KakaoAPI.Search("대구");
+        public static List<Locale> Search(string text)
+        {
+            List<Locale> list = new List<Locale>();
+
+            string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string query = $"{site}?query={text}";
+            string restApiKey = "b9784fcf8a5530f0c231c29f341c63ad";
+            string Header = $"KakaoAK {restApiKey}";
+            //Using System.Net
+            WebRequest request = WebRequest.Create(query);
+            request.Headers.Add("Authorization", Header);
+
+            //응답받기
+            WebResponse response = request.GetResponse();
+            //Using System.IO
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string json = reader.ReadToEnd();
+
+            //using System.Web.Script.Serialization;
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            //dynamic = js에서의 let같은 것
+            dynamic dob = js.Deserialize<dynamic>(json);
+            dynamic docs = dob["documents"];
+            object[] buf = docs;
+            int length = buf.Length;
+            for(int i = 0; i<length; i++)
+            {
+                string lname = docs[i]["place_name"];
+                double x = double.Parse(docs[i]["x"]);
+                double y = double.Parse(docs[i]["y"]);
+                list.Add(new Locale(lname, y, x));
+            }
+            return list;
+
+        }
+    }
+}
