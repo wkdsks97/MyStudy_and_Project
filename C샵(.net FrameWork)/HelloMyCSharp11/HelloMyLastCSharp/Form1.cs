ebb8e59b0bc8b5e455b15ec88ff2dbdb884b52dd
wkdsks97 <109117993+wkdsks97@users.noreply.github.com>
2022-11-29 16:39:23 +0900

    UPLOAD
    2022_11_29

---
+++ C샵(.net FrameWork)/HelloMyCSharp11/HelloMyLastCSharp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyLastCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel1.Text
+                = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+
+            label5.Text = DataManager.Books.Count.ToString();
+            label6.Text = DataManager.Users.Count.ToString();
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text
+                = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+        }
+    }
+}
