93795639d729a56f353cd40932ac86c4a2671886
wkdsks97 <109117993+wkdsks97@users.noreply.github.com>
2022-11-30 09:02:54 +0900

    organize
    organize

---
+++ CLASS/C샵(.net FrameWork)/HelloMyCSharp01/HelloMyCSharp01_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp01_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //java의 nextLine
+            Console.WriteLine("이름이 뭐예요??");
+            String name = Console.ReadLine();
+
+            //java nextInt
+            Console.WriteLine("나이가 어떻게 되세요???");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("몸무게는??????");
+            double kg = double.Parse(Console.ReadLine());
+            Console.WriteLine(name +"님 "+ age + "세 " + kg+"kg");
+            */
+
+           
+        }
+    }
+}
